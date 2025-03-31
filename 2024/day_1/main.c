26846cf63f975322b1cd7c9da646f9379e40bdb3
cash-i1 <117874913+cash-i1@users.noreply.github.com>
2025-03-31 17:52:50 +1100

    2024/day_1: solved

--- 2024/day_1/main.c
+++ 2024/day_1/main.c
@@ -1,16 +1,19 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+
+#define ARRLEN 4096
 
 int main() {
-    int left[128] = {0};
-    int right[128] = {0};
+    int left[ARRLEN] = {0};
+    int right[ARRLEN] = {0};
 
     // read input
     FILE* f = fopen("input.txt", "r");
     assert(f != NULL);
 
-    char line_buf[128] = {0};
+    char line_buf[ARRLEN] = {0};
     int pairs_amt = 0;
     while (fgets(line_buf, sizeof(line_buf), f)) {
         sscanf(line_buf, "%d   %d", &left[pairs_amt], &right[pairs_amt]);
@@ -45,8 +48,21 @@ int main() {
         }
     }
 
-    // print pairs
-    for (int j = 0; j < pairs_amt; j++) {
-        printf("%d, %d\n", left[j], right[j]);
+    // distances
+    int distances[ARRLEN] = {0};
+    for (int i = 0; i < pairs_amt; i++) {
+        // int distance = 0;
+        // if (left[i] > right[i]) distance = left[i] - right[i];
+        // if (right[i] > left[i]) distance = right[i] - left[i];
+        // distances[i] = distance;
+        distances[i] = abs(left[i] - right[i]);
     }
+
+    // total distance
+    int total_distance = 0;
+    for (int i = 0; i < pairs_amt; i++) {
+        total_distance += distances[i];
+    }
+
+    printf("total_distance: %d\n", total_distance);
 }
